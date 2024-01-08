8adcea7728c63215f0f354b4ea04b45669286719
Irene Foster <ifoster@nps.gov>
2024-01-08 11:44:51 -0800

    creating fake data for unit tests

--- R/event_qc.R
+++ R/event_qc.R
@@ -4,13 +4,12 @@
 noSeedlingDataQC <- function() {
 
   noSeedlingFlag <- get_data("Visit")$data$Visit %>%
-    dplyr::select(locationID, eventID) %>%
+    dplyr::select(locationID, eventID, eventDate) %>%
     # Join with seedling data
-    dplyr::full_join(get_data("Seedling")$data$Seedling, by = c('eventID', 'locationID')) %>%
-    dplyr::group_by(locationID, eventID) %>%
-    dplyr::summarise(seedlingCount = dplyr::n()) %>%
+    dplyr::full_join(get_data("Seedling")$data$Seedling, by = c('eventID', 'locationID', 'eventDate')) %>%
+    dplyr::select(locationID, eventID, eventDate, network, park) %>%
     # Filter for events with no corresponding seedling data
-    dplyr::filter(seedlingCount == 0 | is.na(seedlingCount))
+    filter(is.na(network))
 
   return(noSeedlingFlag)
 }
@@ -19,7 +18,7 @@ noSeedlingDataQC <- function() {
 noTreeDataQC <- function() {
 
   noTreeFlag <- get_data("Visit")$data$Visit %>%
-    dplyr::select(locationID, eventID) %>%
+    dplyr::select(locationID, eventDate, eventID) %>%
   # Join with tree data
   dplyr::full_join(get_data("Tree")$data$Tree, by = c('eventID', 'locationID')) %>%
     dplyr::group_by(locationID, eventID) %>%
--- tests/testthat/test-event_qc.R
+++ tests/testthat/test-event_qc.R
@@ -2,25 +2,33 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
+# Load test data
+loadPine("C:/Users/ifoster/Documents/R/mojn-pine-rpackage/tests/testthat/testData", dictionary_dir = "testData/dictionary")
 
 test_that("Test that noSeedlingDataQC() works", {
-  data_dir = "data/final"
-  fiveneedlepine::loadPine("M:/MONITORING/Pine/Data/Database/Backend/FNP_MOJN_Primary.accdb")
+
+  # Compare number of rows returned
+  actual_rows <- nrow(noSeedlingDataQC())
+  expect_equal(actual_rows, 5)
 
   # Compare expected and actual column names
   actual_cols <- colnames(noSeedlingDataQC())
-  expected_cols <- c("locationID", "eventID", "seedlingCount")
+  expected_cols <- c('locationID', 'eventID', 'eventDate', 'network', 'park')
   expect_equal(actual_cols, expected_cols)
 
   # Check that function returns a database
   returnType <- noSeedlingDataQC()
   expect_s3_class(returnType, "data.frame")
+
+  returned_data <- noSeedlingDataQC() %>% arrange(locationID)
+  # check that first row has expected values
+  expect_equal(returned_data[[1,1]], "GRBA_N_010")
+  # Check that eventDate column has correct type
+  expect_s3_class(returned_data$eventDate, "POSIXt")
 })
 
 
 test_that("Test that noTreeDataQC() works", {
-  data_dir = "data/final"
-  fiveneedlepine::loadPine("M:/MONITORING/Pine/Data/Database/Backend/FNP_MOJN_Primary.accdb")
 
   # Compare expected and actual column names
   actual_cols <- colnames(noTreeDataQC())
